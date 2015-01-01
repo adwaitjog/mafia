243002ae19e81216122695b1a2ee7635baf8da4d
tubakesten <tzk133@psu.edu>
2015-01-01 06:29:08 -0500

    Removed folder from repository

--- pthread_benchmark/QTC/libdata.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIBDATA_H_
-#define _LIBDATA_H_
-#include <math.h>
-#include "support.h"
-#include "qtclib.h"
-
-float *generate_synthetic_data(float **rslt_mtrx, int **indr_mtrx, int *max_degree, float threshold, int N, int type);
-
-#endif
